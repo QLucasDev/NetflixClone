80ae9dcb16b9cf13acacacd3a72d1d742636417b
Lucas <qlucas.dev@gmail.com>
2023-01-04 13:34:04 -0300

    Netflix Clone

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "8a52301ef35a646c5da8881ce96bae97"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
